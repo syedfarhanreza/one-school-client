d7eb65ea8c65fcd9d12cb2ea878bcdf2d5067608
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-24 23:58:29 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzQ_Wk1Dlg5Fc15J8c5LnYXQWliXBpmJs",
+  authDomain: "oneschool-32124.firebaseapp.com",
+  projectId: "oneschool-32124",
+  storageBucket: "oneschool-32124.appspot.com",
+  messagingSenderId: "816697978034",
+  appId: "1:816697978034:web:85c60ed47dd6f37b122dab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
