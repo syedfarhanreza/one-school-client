c32cd8fa5ed143e580bcc1d11c2719657c8ba596
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-25 16:08:01 +0600

    routes and layout added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <RouterProvider routes ={routes}></RouterProvider>
     </div>
   );
 }
