a33afc4d7f98fd1ae46686099555a5f9896a8020
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-26 02:49:12 +0600

    navbar added

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/courses')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
     return (
         <div>
-            <h1>This is Courses</h1>
+            <h1>All Course: {courses.length}</h1>
+            <div>
+                {
+                    courses.map(course => <p key={course.id}>
+                        <Link to={`/course/${course.id}`}>{course.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <Navbar collapseOnSelect  expand="lg" bg="dark" variant="dark">
+      <Container className='justify-content-center'>
+        <Navbar.Brand href="#home">OneSchool</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Courses</Nav.Link>
+            <Nav.Link href="#pricing">FAQ</Nav.Link>
+            <Nav.Link href="#pricing">Blog</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
