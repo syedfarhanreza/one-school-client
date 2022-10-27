55a6916ee56b9392e5f134b0d7cb618aff6ae74e
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-27 19:43:00 +0600

    footer added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 import image1 from '../../../assets/images/image1.jpg';
 import image2 from '../../../assets/images/image2.jpg';
 import image3 from '../../../assets/images/image3.png';
 
 const Home = () => {
-    const [index, setIndex] = useState(0);
-  
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
-  
-    return (
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
           <img
@@ -30,7 +32,7 @@ const Home = () => {
             src={image2}
             alt="Second slide"
           />
-  
+
           <Carousel.Caption>
             <h3>Take the world's best courses, online</h3>
             <p >OneSchool provides plenty of information about each class. </p>
@@ -42,16 +44,37 @@ const Home = () => {
             src={image3}
             alt="Third slide"
           />
-  
+
           <Carousel.Caption>
             <h3>OneSchool, For everyone, Forever</h3>
             <p>
-           OneSchool works because it makes coding accessible to any interested student, provides practical recommendations for students who want to learn how to code but don't understand how these new skill might apply to their current job.
+              OneSchool works because it makes coding accessible to any interested student, provides practical recommendations for students who want to learn how to code but don't understand how these new skill might apply to their current job.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-    );
-  };
+      <Card className='mt-4 bg-secondary text-white'>
+        <Card.Header>About Us</Card.Header>
+        <Card.Body>
+          <blockquote className="blockquote mb-0">
+            <p>
+              {' '}
+              OneSchool is a learning site that offers courses from well-known universities. All OneSchool courses contain pre-recorded video lectures that you can watch when it is convenient for you.
+
+              OneSchool has programs together with universities that allow you to get a master degree or specializations. You can explore various college courses without any hassle.
+
+              This site is offering free programs from accredited institutions. The courses available on such platforms are designed by a leading university.
+
+              {' '}
+            </p>
+            <footer className="blockquote-footer">
+              Learn from <cite title="Source Title">OneSchool</cite>
+            </footer>
+          </blockquote>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
 export default Home;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import {  Col, Container, Row } from 'react-bootstrap';
+import { FaDiscord, FaFacebook, FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <Container className='bg-dark' fluid>
-            <Row>
-                <Col>
-                   
-                </Col>
-            </Row>
-        </Container>
+        <footer className="page-footer font-small blue pt-4 bg-dark mt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+                <h5 className="text-uppercase text-white">OneSchool</h5>
+                <p className='text-white'>“The most profound words will remain unread unless you can keep the learner engaged. You can’t see their eyes to know if they got it so … say it, show it, write it, demo it and link it to an activity.” </p>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase">Links</h5>
+                <ul className="list-unstyled">
+                    <li><Link className='text-success'><FaFacebook></FaFacebook> Facebook</Link></li>
+                    <li><Link  className='text-success'><FaGithub></FaGithub> Github</Link></li>
+                    <li><Link  className='text-success'><FaDiscord></FaDiscord> Discord</Link></li>
+                    <li><Link  className='text-success'>About Us</Link></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div className="footer-copyright text-center py-3 text-white">© 2022 Copyright:
+        <Link className='text-decoration-none text-white'> OneSchool</Link>
+    </div>
+
+</footer>
     );
 };
 
