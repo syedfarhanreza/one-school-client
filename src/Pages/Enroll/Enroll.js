25210cbb3ba3ab0e33c191d940299a4757c485ea
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-10-27 19:08:34 +0600

    FAQ added

--- src/Pages/Enroll/Enroll.js
+++ src/Pages/Enroll/Enroll.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 import { useLoaderData } from 'react-router-dom';
 
 const Enroll = () => {
-    const  enrolled = useLoaderData();
-    const {name} = enrolled;
+    const enrolled = useLoaderData();
+    const { name, about,courseBanner } = enrolled;
     return (
-        <div>
-            <h1>{name}</h1>
-        </div>
+       <div>
+         <Card className="text-center mb-5">
+            <Card.Header>OneSchool</Card.Header>
+            <Card.Body>
+                <Card.Title>You have access of {name}</Card.Title>
+                <Card.Text>
+                   {about}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+        <Card>
+        <Card.Img variant="top" style={{height:'350px'}} src={courseBanner} />
+        </Card>
+       </div>
     );
 };
 
--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -1,9 +1,45 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card';
 const FAQ = () => {
     return (
         <div>
-            <h3>This is FAQ</h3>
+            <Card className='mb-4'>
+                <Card.Header as="h5">OneSchool</Card.Header>
+                <Card.Body>
+                    <Card.Title>Q. Can I take more than one course at a time?</Card.Title>
+                    <Card.Text>
+                        If you are new to online learning, we recommend that you begin with one course. As our courses are offered frequently throughout the year, you can always come back and continue your studies with one or more topics.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='mb-4'>
+                <Card.Header as="h5">OneSchool</Card.Header>
+                <Card.Body>
+                    <Card.Title>Q. What should I do if I need to start the course late?</Card.Title>
+                    <Card.Text>
+                        It is recommended that you begin your course promptly. If you do need to start the course a few days late, contact your instructor as soon as possible after the course has opened to discuss your options. Registration closes on the Friday after the course begins.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='mb-4'>
+                <Card.Header as="h5">OneSchool</Card.Header>
+                <Card.Body>
+                    <Card.Title>Q. When should I register?</Card.Title>
+                    <Card.Text>
+                        Most courses are offered at least 3 times during the year. Certain courses can fill up quickly, so be sure to register early.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='mb-4'>
+                <Card.Header as="h5">OneSchool</Card.Header>
+                <Card.Body>
+                    <Card.Title>Q. Is there an orientation for online students?</Card.Title>
+                    <Card.Text>
+                        Two weeks before your course starts you will be invited to join the SIA Online Community where our guides will introduce you to the technology and dynamics of online teaching and learning. It also gives you a chance to introduce yourself and network with fellow students.
+                        Should you have any questions or need help troubleshooting a piece of technology, our Online Team is only an email away and is ready to assist.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
